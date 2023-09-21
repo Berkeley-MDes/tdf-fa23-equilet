e55a95a90827ad3767d979bbb576d790d6800cdb
equilet <2237372+equilet@users.noreply.github.com>
2023-09-21 11:39:10 -0700

    adding particle workbench series

---
+++ project_demonstrables/particle_workbench/basic_button_soc/src/basic_button_soc.ino
@@ -0,0 +1,45 @@
+/*
+ * Project basic_button_pw
+ * Description: button config for p5js web sketch 
+ * Author: Jeff Lubow 
+ * Date: 9/17/23
+ */
+
+int button = D10; // button leg is connected to D10
+int ledpin = D7; 
+const char *be = "button_event"; //the name of our webhook event
+volatile int state = 0;
+volatile int state_was = 0;
+
+void button_update(void);
+ 
+void setup(void) {
+  Serial.begin(9600);   // We'll send debugging information via the Serial monitor
+  pinMode(ledpin, OUTPUT); // sets pin as output
+  pinMode(button, INPUT_PULLDOWN); // sets pin as input; defaults to low state
+}
+ 
+void button_update(void) {
+  String state_str = String(state);
+  digitalWrite(ledpin, state); // sets the LED on  
+  Particle.publish(be, state_str, PRIVATE);
+}
+
+//the fast stuff
+void loop(void) {
+  state = digitalRead(button);
+  String state_str = String(state);
+
+  Serial.printf("state: %d\n", state);
+  // String state_str = String(state);
+  // digitalWrite(ledpin, state); // sets the LED on  
+  if(state == 1 && state_was == 0) {
+    Particle.publish(be, state_str, PRIVATE);
+  } 
+  if(state == 0 && state_was == 1) {
+    Particle.publish(be, state_str, PRIVATE);
+  }
+
+  state_was = state;
+  delay(100);
+}
